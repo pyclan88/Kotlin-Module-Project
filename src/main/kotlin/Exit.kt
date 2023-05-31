dd260cabf24a073237b0833b7333ce541a5b0907
pyclan88 <gumabemc@yandex.ru>
2023-05-31 09:55:10 +0300

    Исправлены все замечания

--- src/main/kotlin/Exit.kt
+++ src/main/kotlin/Exit.kt
@@ -1,21 +1,22 @@
 import menu.FileMenu
 import menu.FolderMenu
 import kotlin.system.exitProcess
+import Exit.*
 
 enum class Exit {
     FROM_FOLDER_MENU,
     FROM_FILE_MENU,
     FROM_NOTE_MENU;
+}
 
-    companion object {
-        internal var status = FROM_FOLDER_MENU
+object ExitRouter {
+    internal var status = FROM_FOLDER_MENU
 
-        internal fun executeExit() {
-            when (Exit.status) {
-                FROM_FOLDER_MENU -> exitProcess(0)
-                FROM_FILE_MENU -> FolderMenu().showMenu()
-                FROM_NOTE_MENU -> FileMenu().showMenu()
-            }
+    internal fun executeExit() {
+        when (status) {
+            FROM_FOLDER_MENU -> exitProcess(0)
+            FROM_FILE_MENU -> FolderMenu().showMenu()
+            FROM_NOTE_MENU -> FileMenu().showMenu()
         }
     }
 }
---
+++ src/main/kotlin/FolderRepository.kt
@@ -0,0 +1,5 @@
+class FolderRepository {
+    companion object {
+        internal val archive: MutableList<Folder> = mutableListOf()
+    }
+}
